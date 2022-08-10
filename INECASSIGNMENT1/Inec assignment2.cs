01f80f8549a8f0bf534a28e2c89d86144da3aaa9
Support <Support@WEMA-TRAINING-S>
2022-08-10 10:47:40 +0100

    Add project files.

---
+++ INECASSIGNMENT1/Inec assignment2.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace INECASSIGNMENT1
+{
+    internal class Inec_assignment2
+    {
+        public void Inec()
+        {
+            SortedSet<string> sortedset_inec = new SortedSet<string>();
+            sortedset_inec.Add("ayo");
+            sortedset_inec.Add("kingsley");
+            sortedset_inec.Add("ibrahim");
+            sortedset_inec.Add("odunayo");
+            sortedset_inec.Add("ike");
+            sortedset_inec.Add("hassan");
+            sortedset_inec.Add("tobi");
+            sortedset_inec.Add("pius");
+            sortedset_inec.Add("mariam");
+
+            Console.WriteLine("Enter your name");
+           string userName= Console.ReadLine();
+            userName = userName.ToLower(); //change every entry to lowercase
+            if (sortedset_inec.Contains(userName))
+            {
+              Console.WriteLine("Access Denied");
+            }
+            else
+            {
+                sortedset_inec.Add(userName);
+                Console.WriteLine("User added successfully");
+            }
+
+        }
+    }
+}
---
+++ INECASSIGNMENT1/Program.cs
@@ -0,0 +1,48 @@
+﻿namespace INECASSIGNMENT1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Inec_assignment2 inec_assignment = new Inec_assignment2();
+
+            inec_assignment.Inec();
+            Dictionary<int, string> InecRegistration = new Dictionary<int, string>()
+        {
+            {1, "Adeola Aderenle" },
+            {2, "Temidayo Okeowo" },
+            {3, "Odunayo Abegunde"},
+            {4, "Ikechukwu Bright"},
+            {5,"Matthew Timadi" },
+            {6, "Temidayo Okeowo" },
+            {7,"Florence Ikpamo" },
+            {8,"Bola Ige"},
+            {9, "Mustapha Idris"  },
+            {10, "Odunayo Abegunde" },
+
+        };
+            Console.WriteLine("Voters Registration ");
+
+            foreach (KeyValuePair<int, string> dups in InecRegistration) //Go through the array and check for duplicate 
+            {
+                Console.WriteLine(dups.Key + "  " + dups.Value);
+            }
+            var duplicateValues = InecRegistration.GroupBy(x => x.Value).Where(x => x.Count() > 1); //Group the duplicate names
+
+            Console.WriteLine("The Duplicates are: ");
+
+            foreach (var name in duplicateValues)   //Prints out the duplicate names
+            {
+                Console.WriteLine(name.Key);
+            }
+
+
+
+
+            
+
+
+        }
+    }
+}
